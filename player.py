71c2762e3a096a2607034c898b945c1f15ea3d63
ethanewl <ethanlong47@gmail.com>
2026-01-28 08:25:41 +0000

    updated player class to include w s key movement

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -21,6 +21,12 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
         
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        vector_rotate = unit_vector.rotate(self.rotation)
+        vector_lenth = vector_rotate * (PLAYER_SPEED * dt)
+        self.position += vector_lenth
+        
     def update(self, dt):
         keys = pygame.key.get_pressed()
         
@@ -29,3 +35,10 @@ class Player(CircleShape):
         
         if keys[pygame.K_d]:
             self.rotate(dt)
+            
+        if keys[pygame.K_w]:
+            self.move(dt)
+        
+        if keys[pygame.K_s]:
+            self.move(-dt)
+            
