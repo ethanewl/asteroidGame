14422c1342ab822fb3972a4e3daf3061de0840b3
ethanewl <ethanlong47@gmail.com>
2026-01-28 08:11:15 +0000

    added circle class and player object. updated main to draw sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 import pygame
 
 def main():
@@ -8,8 +9,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
+        
         log_state()
         
         for event in pygame.event.get():
@@ -17,11 +20,13 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         
         dt = clock.tick(60)/1000
 
+        
 
 
 if __name__ == "__main__":
